ce4c00c82b2cb09edeb6b84186e6d3069903680d
DEF4LT-303 <ryanrafi72@gmail.com>
2023-11-23 14:06:00 +0600

    profile section updated/gallery need fix

--- client/src/app/(pages)/profile/page.jsx
+++ client/src/app/(pages)/profile/page.jsx
@@ -1,9 +1,10 @@
 'use client';
 
+import Gallery from '@/app/components/Gallery';
 import { useDispatch, useSelector } from 'react-redux';
 
 const Profile = () => {
-  const user = useSelector((state) => state.user.currentUser);
+  const user = useSelector((state) => state.user.currentUser.user);
   const loading = false;
 
   const dispatch = useDispatch();
@@ -31,31 +32,45 @@ const Profile = () => {
 
   return (
     <>
-      <div className='relative'>
+      <div className='relatiive'>
         <img
           src='https://t3.ftcdn.net/jpg/05/78/94/94/360_F_578949486_IUFDkbOIhPmDkiKwCNvG7oOAtP9sNvO6.jpg'
           alt='background'
-          className='px-5 py-5 h-[200px] w-full bg-center bg-cover contrast-75 filter'
+          className='px-5 py-5 h-[200px] w-full bg-center bg-cover contrast-75 filter relative'
         />
 
-        <div className='container mx-auto mt-8 relative sm:absolute'>
-          <div className='flex flex-col sm:flex-row gap-2 items-center mt-[-90px] px-5 ml-5'>
-            <div className='w-20 h-20 rounded-full overflow-hidden border-4 border-white ring ring-offset-2 ring-offset-primary'>
+        <div className='mx-auto mt-8'>
+          <div className='flex flex-col sm:flex-row gap-2 items-center px-5 ml-5 mt-[-105px]'>
+            <div className='w-20 h-20 z-20 rounded-full overflow-hidden border-4 border-white ring ring-offset-2 ring-offset-primary'>
               <img
                 src={user?.photo || '/default.jpg'}
                 alt='avatar'
                 className='w-full h-full object-cover'
               />
             </div>
-            <div className='flex flex-col gap-1 ml-2 mt-2 sm:mt-10'>
+            <div className='flex flex-col sm:flex-row w-full justify-center items-center sm:justify-between gap-1 ml-2 mt-2 sm:mt-10'>
               <div className='text-white font-bold text-xl'>
                 {user?.firstName || 'User'}
               </div>
+              <div className='flex flex-row gap-3 mt-6'>
+                <div>
+                  <button className='btn btn-primary w-32'>Edit Profile</button>
+                </div>
+                <div>
+                  <button className='btn btn-primary w-32'>Delete</button>
+                </div>
+              </div>
             </div>
-
-            {/* Add the rest of your profile details here */}
           </div>
         </div>
+
+        <hr className='my-6 mx-5 border-gray-200 md:my-10 dark:border-gray-700' />
+
+        <div className='text-center'>
+          <h1 className='text-2xl font-bold'>Gallery</h1>
+        </div>
+
+        <Gallery />
       </div>
     </>
   );
--- client/src/app/components/Gallery.jsx
+++ client/src/app/components/Gallery.jsx
@@ -1,6 +1,5 @@
 const Gallery = () => {
   const imageUrls = [
-    'https://source.unsplash.com/random/301x301/',
     'https://source.unsplash.com/random/200x200/?0',
     'https://source.unsplash.com/random/200x200/?1',
     'https://source.unsplash.com/random/200x200/?2',
@@ -9,23 +8,22 @@ const Gallery = () => {
     'https://source.unsplash.com/random/200x200/?5',
     'https://source.unsplash.com/random/200x200/?6',
     'https://source.unsplash.com/random/200x200/?7',
-    'https://source.unsplash.com/random/302x302/',
-    'https://source.unsplash.com/random/200x200/?7',
-    'https://source.unsplash.com/random/200x200/?7',
-    'https://source.unsplash.com/random/200x200/?7',
-    'https://source.unsplash.com/random/200x200/?7'
+    'https://source.unsplash.com/random/200x200/?8',
+    'https://source.unsplash.com/random/200x200/?9'
   ];
 
   return (
-    <section className='py-6 dark:bg-gray-800 dark:text-gray-50'>
+    <section className='py-6 dark:bg-base-100 dark:text-gray-50'>
       <div className='container grid grid-cols-2 gap-4 p-4 mx-auto md:grid-cols-4'>
         {imageUrls.map((url, index) => (
           <img
             key={index}
             alt=''
             className={`w-full h-full rounded shadow-sm min-h-48 dark:bg-gray-500 aspect-square ${
-              index % 5 === 0 && index !== imageUrls.length - 1
-                ? 'col-span-2 row-span-2 md:col-span-2 md:row-span-2'
+              index % 5 == 0
+                ? 'col-span-2 row-span-2 md:col-start-3 md:row-start-1'
+                : index % 10 === 0
+                ? 'col-span-2 row-span-2 md:col-start-1 md:row-start-3'
                 : ''
             }`}
             src={url}
