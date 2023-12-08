16f31ef1561a8b02815dac1b54e51448a251a8c9
DEF4LT-303 <ryanrafi72@gmail.com>
2023-12-08 16:09:12 +0600

    like post fixed

--- client/src/app/(pages)/page.jsx
+++ client/src/app/(pages)/page.jsx
@@ -1,6 +1,17 @@
+'use client';
+
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { getPosts } from '../api/redux/apiCalls';
 import Header from '../components/Header';
 
 const page = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    getPosts(dispatch);
+  }, [dispatch]);
+
   return (
     <div>
       <Header />
